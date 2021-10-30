ef40eb07b059bf32ad43df6e9f13c14cc9ac279e
Ramasahayam Sreekaran Reddy <86601811+Sreekaran1704@users.noreply.github.com>
2021-10-31 00:06:06 +0530

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+    //cout<<"Hello world harry"<<endl;
+    //cout<<"NextLine";
+    //int marks=98;
+    //cout<<"The total marks are : "<<marks;
+
+
+
+    // int a,b;
+    // cout<<"Enter the first number : "<<endl;
+    // cin>>a;
+    // cout<< "Enter the second number : "<<endl;
+    // cin>>b;
+    // cout<<"The sum is : " <<a+b<< endl;
+
+
+    //conditional state ments
+    // int a;
+    // cout<<"Enter your age : "<<endl;
+    // cin>>a;
+    // if(a>=18){
+    //     cout<<"You are eligible to vote";
+    // }else{
+    //     cout<<"You are not eligible to vote";
+    // }
+    // return 0;
+
+
+
+    // loops
+    //1D -array
+    // int n;
+    // cout<<"Enter the size :"<<endl;
+    // cin>>n;
+    // int array[n];
+    // for(int i=0;i<n;i++){
+    //     cout<<"Enter your "<<i+1<<" th Number :"<<endl;
+    //     cin>>array[i];
+    // }
+    // for(int i=0;i<n;i++){
+    //     cout<<array[i]<<endl;
+    // }
+    // return 0;
+
+
+    //2D -array
+    // int rows,columns;
+    // cout<<"Enter the number of rows : ";
+    // cin>>rows;
+    // cout<<"Enter the number of columns : ";
+    // cin>>columns;
+    // int array[rows][columns];
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<columns;j++){
+    //         cout<<"Enter the values : "<<endl;
+    //         cin>>array[i][j];
+    //     }
+    // }
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<columns;j++){
+    //         cout<<array[i][j]<<" ";
+    //     }cout<<endl;
+    // }
+
+    //Strings 
+
+    string name="Sree karan";
+    cout<<name.length()<<endl;
+    cout<<name<<endl;
+}
